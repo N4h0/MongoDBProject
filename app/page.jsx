32109dd5131766938fd74fa1f3596814383c739f
N4h0 <89570972+N4h0@users.noreply.github.com>
2024-11-08 01:05:58 +0100

    Added dropdown menus

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import TestButton from "./clientSideComponents/clientSideComponents"; // Import the client-side component
+import TestButton from "./clientSideComponents/hentAllData"; // Import the client-side component
+import DropDownCollection from "./clientSideComponents/dropDownCollection";
+import DropDownDatabases from "./clientSideComponents/dropDownCollection";
+
 
 export default async function Home() {
   return (
     <>
+      <DropDownDatabases/>
+      <p></p>
+      <DropDownCollection/>
+
       <h1>"Hei, dette er frammesida. Me bruker app routing fordi serverside routing ftw."</h1>
       <h1>"Desverre må me ha med nokre client-side komponentat, feks knappen under :("</h1>
-      <TestButton/>
+      <TestButton />
       <p></p>
       <Link href="/api/testConnection" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
         <code>Test autorisasjon til database (link)</code>
